7efc5161c57539d28521f50ce2de6211dc0ebae5
Lusty94 <108025667+Lusty94@users.noreply.github.com>
2022-09-25 17:04:37 +0100

    new EN translations

--- locales/en.lua
+++ locales/en.lua
@@ -1,14 +1,14 @@
 local Translations = {
     error = {
-        ["missing_something"] = "It looks like you are missing something...",
-        ["not_enough_police"] = "Not enough Police..",
-        ["door_open"] = "The door is already open..",
-        ["process_cancelled"] = "Process Canceled..",
-        ["didnt_work"] = "It did not work..",
-        ["emty_box"] = "The Box Is Empty..",
+        ["missing_something"] = "You Don't Have The Required Items!",
+        ["not_enough_police"] = "There Are Not enough Police On Duty!",
+        ["door_open"] = "The Door Has Already Been Opened!",
+        ["process_cancelled"] = "Your Hands Slipped & You Made A Noise!",
+        ["didnt_work"] = "You Broke The Lockpick!",
+        ["emty_box"] = "You Found Nothing Here!..",
     },
     success = {
-        ["worked"] = "It worked!",
+        ["worked"] = "It worked! Now Be quick!",
     }
 }
 
